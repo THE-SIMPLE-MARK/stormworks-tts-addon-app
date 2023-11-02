c90deba5d69c5ab0566e52c8b46fcbd6d179c2ba
THE-SIMPLE-MARK <57036815+THE-SIMPLE-MARK@users.noreply.github.com>
2023-11-02 16:07:07 +0100

    Add default css resets & next-themes for auto theming

--- renderer/pages/_app.jsx
+++ renderer/pages/_app.jsx
@@ -1,10 +1,13 @@
 import "../styles/globals.css"
 import { NextUIProvider } from "@nextui-org/react"
+import { ThemeProvider } from "next-themes"
 
 export default function MyApp({ Component, pageProps }) {
 	return (
 		<NextUIProvider>
-			<Component {...pageProps} />
+			<ThemeProvider attribute="class" enableSystem enableColorScheme>
+				<Component {...pageProps} />
+			</ThemeProvider>
 		</NextUIProvider>
 	)
 }
--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,5 +1,9 @@
 import React from "react"
 import { Button } from "@nextui-org/react"
+import Head from "next/head"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export default function HomePage() {
 	const [message, setMessage] = React.useState("No message found")
@@ -10,5 +14,16 @@ export default function HomePage() {
 		})
 	}, [])
 
-	return <h1 className="text-3xl font-bold underline">Hello, world!</h1>
+	return (
+		<>
+			<Head>
+				<title>Stormworks TTS Companion</title>
+			</Head>
+			<main
+				className={`${inter.className} w-screen h-screen text-foreground bg-background`}
+			>
+				<Button>test</Button>
+			</main>
+		</>
+	)
 }
